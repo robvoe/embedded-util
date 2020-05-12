6ae476cde5f79c539b6fb2bd35f0784eedb7af29
Robert Voelckner <robert.voelckner1@mailbox.tu-dresden.de>
2020-05-12 16:33:10 +0200

    Fixed a small "could have become" bug: The ArmInterruptPreventionMutex now is able to reset the interrupt flag to how it was before.

--- Mutex/ArmInterruptPreventionMutex.h
+++ Mutex/ArmInterruptPreventionMutex.h
@@ -5,7 +5,8 @@
  *      Author: Robert Voelckner
  *
  *  Description:
- *    Implements the MutexBase class. Prevents all ARM interrupts from being executed.
+ *    Implements the MutexBase class. Prevents all ARM Cortex-M interrupts (except for NMI and hard fault) from being executed.
+ *    Important: Enabling/disabling interrupts can only performed by software which runs in privileged mode.
  */
 #ifndef APPLICATION_USER_UTIL_MUTEX_ARMINTERRUPTPREVENTIONMUTEX_H_
 #define APPLICATION_USER_UTIL_MUTEX_ARMINTERRUPTPREVENTIONMUTEX_H_
@@ -21,13 +22,35 @@ namespace Util {
 
 		class ArmInterruptPreventionMutex : public MutexBase {
 
+				/// The following switch defines if, upon releasing the Mutex, the previous state shall be restored. If not,
+				/// the interrupts will just be enabled again - with no regard to whether they were or were not enabled before.
+				#if not defined ARM_INTERRUPT_PREVENTION_MUTEX__RESTORE_PREVIOUS_STATE
+					static constexpr bool RestorePreviousState = true;
+				#else
+					static constexpr bool RestorePreviousState = ARM_INTERRUPT_PREVENTION_MUTEX__RESTORE_PREVIOUS_STATE;
+				#endif
+
+
+				uint32_t _previousPrimaskState;
+
 			public:
 				ArmInterruptPreventionMutex() {
+					if ( RestorePreviousState ) {
+						_previousPrimaskState = __get_PRIMASK();
+					}
 					__disable_irq();
 				}
 
 				~ArmInterruptPreventionMutex() {
-					__enable_irq();
+					if ( RestorePreviousState ) {
+						/*bool interruptsWereDisabled = _previousPrimaskState & UINT32_C(1);
+						if ( interruptsWereDisabled )   __disable_irq();
+						else                            __enable_irq();*/
+						__set_PRIMASK(_previousPrimaskState);
+					}
+					else if ( !RestorePreviousState ) {
+						__enable_irq();
+					}
 				}
 
 		};
